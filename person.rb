41bd0bd8d8328d5a68e4e9395b393ddabe302932
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-26 15:44:11 +0000

    Seperate classes form from main.rb

---
+++ person.rb
@@ -0,0 +1,21 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Ramdom.rand(0..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
