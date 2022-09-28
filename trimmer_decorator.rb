7bfd24a93652daa2c4cdd9c7fe69683ffbf86f09
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-28 12:48:47 +0000

    Create trimmer  decolator class

---
+++ trimmer_decorator.rb
@@ -0,0 +1,11 @@
+require_relative 'nameable'
+
+class TrimmerDecorator < Nameable
+    def initialize(nameable)
+      @nameable = nameable
+    end
+  
+    def correct_name
+      @nameable.correct_name[0..9]
+    end
+  end
