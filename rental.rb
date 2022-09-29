a1737d0fd61d205b927724ea71ea6c454c45424d
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-29 23:59:47 +0000

    Modify rental class

--- rental.rb
+++ rental.rb
@@ -1,11 +1,14 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
-    book.rentals << self
-    person.rentals << self
+    # Add the rental to the book and person
+    puts @book
+    puts @person
+    @book.add_rental(self)
+    @person.add_rental(self)
   end
 end
