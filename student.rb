35f43b76a86941558a0f4cafbe5c503f4a5bc7e5
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-29 13:08:07 +0000

    Make add_classroom method on it own

--- student.rb
+++ student.rb
@@ -3,8 +3,11 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
-    # add student to a classroom when setting classroom for a student
-    classroom.add_student(self)
+  end
+
+  def add_classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hook
