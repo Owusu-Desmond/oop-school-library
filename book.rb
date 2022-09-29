67101f1f6d508f04c754143c7081332e8588203d
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-29 23:58:44 +0000

    Modify book class

--- book.rb
+++ book.rb
@@ -1,5 +1,3 @@
-require_relative 'rental'
-
 class Book
   attr_accessor :title, :author
 
@@ -9,8 +7,7 @@ class Book
     @rentals = []
   end
 
-  def add_rental(person, date)
-    Rental.new(date, self, person)
-    @rentals.push(rental)
+  def add_rental(rental)
+    @rentals << rental
   end
 end
