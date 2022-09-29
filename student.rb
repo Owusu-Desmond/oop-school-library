f77397c52682646cf05e89b753a93bebe33f496c
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-29 11:20:38 +0000

    Modifly student class

--- student.rb
+++ student.rb
@@ -3,6 +3,8 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    # add student to a classroom when setting classroom for a student
+    classroom.add_student(self)
   end
 
   def play_hook
