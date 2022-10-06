1789787d9ccb24f8be8457a2d73c515ecc23c66e
Owusu-Desmond <desmond0406114009@gmail.com>
2022-10-06 08:32:48 +0000

    Write test for Person class

---
+++ RSpec/person.spec.rb
@@ -0,0 +1,18 @@
+require_relative '../person'
+describe Person do
+  let(:person) { Person.new(name: 'John', age: 18) }
+
+  describe '#new' do
+    it 'creates a new person' do
+      expect(person).to be_a(Person)
+    end
+
+    it 'has a name' do
+      expect(person.name).to eq('John')
+    end
+
+    it 'has an age' do
+      expect(person.age).to eq(18)
+    end
+  end
+end
